9b457af4afd02bdfb8c5119145716670594549be
jrojas2016 <jrojas2016@gmail.com>
2015-10-25 02:22:03 -0400

    Database added with working login

--- server/app.py
+++ server/app.py
@@ -9,20 +9,26 @@ Description:
 author(s):
 	Jorge Rojas
 '''
+import smtplib
+import hashlib
+import datetime
 from sys import path
 from os import getcwd
 path.append(getcwd())
-from scripts.updateAsana import updateAsana
-from scripts.createAgendaFromAsana import createAgenda
+from pymongo import MongoClient
+# from scripts.updateAsana import updateAsana
+# from scripts.createAgendaFromAsana import createAgenda
 from flask import Flask, request, render_template
-
-TOTAL_ROUTES = 3
-APP_URL = 'http://127.0.0.1:5000'
 
 app = Flask(__name__)
 
-class pageElements:
+'''MongoDB Client & Collections'''
+client = MongoClient()
+va_db = client['virtual-admin-db']
+c_chapters = va_db['chapters']
 
+class pageElements:
+	usr = None
 	error = None
 
 	def __init__(self, title, parent = '/'):
@@ -32,61 +38,154 @@ class pageElements:
 	def changeParent(self, parent):
 		self.parent = parent
 
-def user_confirmed(usrEmail):
-	return True
+def sendEmailConfirmation(postForm):
+	#Implement email confirmation after sign-up
+	sender = 'va.test2016@gmail.com'
+	passwrd = 'VAsorullit0'
+	server = smtplib.SMTP("smtp.gmail.com",587)
+	server.starttls()
+	server.login(sender, passwrd)
+	send_email = postForm['email']
+	send_name = send_email.split('@')[0]
+	msg = [ 
+		'From: %s'%sender, 
+		'To: %s'%send_email,
+		'Subject: Virtual Admin - Email Confirmation',
+		'',
+		'Thanks for joining Virtual Admin. To confirm your email please click the link below:',
+		'',
+		'http://virtualadmin.herokuapp.com/confirm/%s'%send_name,
+		'',
+		'Sincerely,',
+		'Virtual Admin Team'
+	]
 
-def valid_login(usrEmail, passWord):
+	message = '\r\n'.join( msg )
+
+	try:
+		server.sendmail( sender, send_email, message )
+		print 'Email sent successfully to %s'%postForm['email']
+	except:
+		print 'Email failed to send to %s!'%postForm['email']
+
+	server.quit()
+
+def signUpUser(postForm):
+	newUser = True
+	usr_email = postForm['email']
+	usr_name = usr_email.split('@')[0]
+	usr_psswrd = postForm['password']
+	post = {
+		'email': usr_email,
+		'name': usr_name,
+		'password': hashlib.sha512(usr_psswrd).hexdigest(),
+		'joined-on': datetime.datetime.utcnow(),
+		'email_confirmed': False,
+		'integrations': [ None ],
+		'organization': None
+	}
+
+	for cur_chapter in c_chapters.find():
+		if cur_chapter['email'] == usr_email:
+			newUser = False
+
+	if newUser:
+		c_chapters.insert_one(post)
+		status = None
+	elif newUser and usr_psswrd == None:
+		status = 'Password is a required field!'
+	elif not newUser and usr_psswrd == None:
+		status = 'Password is a required field!'
+	else:
+		status = 'There already exists an account with the email %s. Please Log in here: http://virtualadmin.herokuapp.com/login'%usr_email
+
+	return status
+
+
+def validLogin(postForm):
 	'''
 	Given email and password, authorize user 
 	to access Virtual Admin Dashboard
 	'''
-	if usrEmail == 'jrojas@stevens.edu':
-		return True
-	else:
-		return False
+	psswrd = hashlib.sha512(postForm['password']).hexdigest()
+	for cur_chapter in c_chapters.find():
+		if postForm['email'] == cur_chapter['email'] and psswrd == cur_chapter['password']:
+			if cur_chapter['email_confirmed'] == True:
+				return True
+			else:
+				return False
+		else:
+			return False
 
 @app.route('/')
 def renderLanding():
 	landingElements = pageElements('Virtual Admin Landing Page')
 	return render_template('landingPage.html', pgElements = landingElements)
-	# return 'LUL AA Admin Landing Page'
+
+@app.route('/confirm/<usrName>')
+def confirmUser(usrName = None):
+
+	res = c_chapters.update(
+		{
+			"name": usrName
+		},
+		{ 
+			"$set": {
+				"email_confirmed": True
+			} 
+		}
+	)
+	print res
+	if res['updatedExisting'] == True:
+		print 'User was successfully confirmed!'
+	else:
+		print 'Failed to confirm user!'
+
+	loginElements = pageElements('Virtual Admin Log-in Page', parent = '/login')
+	return render_template('loginPage.html', pgElements = loginElements)
 
 @app.route('/createAgenda')
 def runCreateAgenda():
-	createAgenda()
+	# createAgenda()
 	#Return page after createAgenda() finishes execution
 	homeElements = pageElements('Virtual Admin Dashboard')
 	return render_template('homePage.html', pgElements = homeElements)
 
-@app.route('/home')
-def renderHome():
-	homeElements = pageElements('Virtual Admin Dashboard', parent = '/home')
-	return render_template('homePage.html', pgElements = homeElements)
-	# return 'LUL AA Admin Home Page'
+@app.route('/dashboard/<name>')
+def renderDashboard(name = None):
+	homeElements = pageElements('Virtual Admin Dashboard', parent = '/dashboard')
+	homeElements.usr = name
+	return render_template('dashboardPage.html', pgElements = homeElements)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def renderLogin():
 	loginElements = pageElements('Virtual Admin Log-in Page', parent = '/login')
 	if request.method == 'POST':
-		if valid_login(request.form['email'], request.form['password']) and user_confirmed(request.form['email']):
-			return renderHome()
+		if validLogin(request.form):
+			return renderDashboard()
 		else:
 			loginElements.error = 'ERROR: Invalid username/password'
 	
-	return render_template('loginPage.html', pgElements=loginElements)
-	# return 'LUL AA Admin Login Page'
+	return render_template('loginPage.html', pgElements = loginElements)
 
 @app.route('/signUp', methods = ['GET','POST'])
-def signUpUser():
+def signUp():
 	signUpElements = pageElements('Virtual Admin Sign-Up Page', parent = '/signUp')
 	if request.method == 'POST':
-		return render_template('signUpConfirmationPage.html')
+		status = signUpUser(request.form)
+		if status:
+			loginElements = pageElements('Virtual Admin Sign-Up Page', parent = '/login')
+			loginElements.error = status
+			return render_template('loginPage.html', pgElements = loginElements)
+		else:
+			sendEmailConfirmation(request.form)
+			return render_template('signUpConfirmationPage.html')
 	else:
-		return render_template('loginPage.html', pgElements=signUpElements)
+		return render_template('loginPage.html', pgElements = signUpElements)
 
 @app.route('/updateAsana')
 def runUpdateAsana():
-	updateAsana()
+	# updateAsana()
 	#Return page after updateAsana() finishes execution
 	homeElements = pageElements('Virtual Admin Dashboard')
 	return render_template('homePage.html', pgElements = homeElements)
